ab130be517f39efb9d65c04788236ad3019c9a79
nirtzur <nirtzur89@gmail.com>
2019-08-28 19:50:16 +0200

    basics

---
+++ notes-app.js
@@ -0,0 +1,59 @@
+const notes = getSavedNotes();
+
+const filters = {
+	searchText: ''
+};
+
+renderNotes(notes, filters);
+
+//buttons
+//search button
+document.querySelector('#search-note').addEventListener('input', function(e) {
+	filters.searchText = e.target.value;
+	renderNotes(notes, filters);
+});
+//create btn
+document.querySelector('#create-note').addEventListener('click', function() {
+	notes.push({
+		id: uuidv4(),
+		title: '',
+		body: ''
+	});
+	saveNotes(notes);
+	renderNotes(notes, filters);
+});
+
+document.querySelector('#filter-by').addEventListener('change', function(e) {
+	console.log(e.target.value);
+});
+
+// //submit event
+// document.querySelector('#name-form').addEventListener('submit', function(e) {
+// 	e.preventDefault();
+// 	alert(e.target.elements.firstName.value);
+// 	e.target.elements.firstName.value = '';
+// });
+
+// //remove all btn
+// document.querySelector('#remove-all').addEventListener('click', function() {
+// 	document.querySelectorAll('.note').forEach(function(note) {
+// 		note.remove();
+// 	});
+// });
+
+// // //query and remove
+// // const p = document.querySelector('p');
+// // p.remove();
+
+// //query all and remove
+// const ps = document.querySelectorAll('p');
+// ps.forEach(function(p) {
+// 	p.textContent = '******';
+// 	//console.log(p.textContent);
+// 	//p.remove();
+// });
+
+// //add a new element
+// const newPar = document.createElement('p');
+// newPar.textContent = 'this is a new Par from JS';
+// document.querySelector('body').appendChild(newPar);
---
+++ notes-functions.js
@@ -0,0 +1,63 @@
+//read existing existing saved data
+const getSavedNotes = function() {
+	const notesJSON = localStorage.getItem('notes');
+
+	if (notesJSON !== null) {
+		return JSON.parse(notesJSON);
+	} else {
+		return [];
+	}
+};
+
+//save notes to local storage
+const saveNotes = function(notes) {
+	localStorage.setItem('notes', JSON.stringify(notes));
+};
+
+//remove note function
+const removeNote = function(id) {
+	const noteIndex = notes.findIndex(function(note) {
+		return note.id === id;
+	});
+	if (noteIndex > -1) {
+		notes.splice(noteIndex, 1);
+	}
+};
+
+//generate the DOM stracture for a note
+const generateNoteDOM = function(note) {
+	const noteEl = document.createElement('div');
+	const textEl = document.createElement('span');
+	const button = document.createElement('button');
+
+	//remove note bttn
+	button.textContent = 'x';
+	noteEl.appendChild(button);
+	button.addEventListener('click', function() {
+		removeNote(note.id);
+		renderNotes(notes, filters);
+	});
+
+	//setup note title text
+	if (note.title.length > 0) {
+		textEl.textContent = note.title;
+	} else {
+		textEl.textContent = 'Unnamed Note';
+	}
+	noteEl.appendChild(textEl);
+
+	return noteEl;
+};
+
+//render app notes
+const renderNotes = function(notes, filters) {
+	const filteredNotes = notes.filter(function(note) {
+		return note.title.toLowerCase().includes(filters.searchText.toLowerCase());
+	});
+	document.querySelector('#notes').innerHTML = '';
+
+	filteredNotes.forEach(function(note) {
+		const noteEl = generateNoteDOM(note);
+		document.querySelector('#notes').appendChild(noteEl);
+	});
+};
